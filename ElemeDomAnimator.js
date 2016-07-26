b6a814a382b491aadcf13762ec612dd88fcacb82
李雁楠 <yannan.li@ele.me>
2016-07-26 12:14:31 +0800

    ADD: add the basic function

---
+++ ElemeDomAnimator.js
@@ -0,0 +1,51 @@
+var React = require('react');
+var DomAnimator = require('./domAnimator.src');
+
+var ElemeDomAnimator = React.createClass({
+  render: function() {
+    const domAnimator = new DomAnimator();
+
+    // The graph below will show in the console
+    const burger = ['',
+      '  ___________ ',
+      ' /           \\',
+      '|_____________|',
+      ' |___________|',
+      '_|___________|_',
+      '\\_____________/'];
+
+    const drink = ['       ||     ',
+      '  _____||_____ ',
+      ' /____________\\',
+      '  \\          /',
+      '   \\        /',
+      '    \\      /',
+      '     \\____/'];
+
+    const melon = ['       /\\     ',
+      '      /..\\     ',
+      '     /....\\     ',
+      '    /......\\     ',
+      '   /........\\     ',
+      '  /\\________/\\     ',
+      '  \\__________/     '];
+
+    const icecream = ['',
+      '        () ',
+      '       (__) ',
+      '     (______) ',
+      '      \\    /',
+      '       \\  /',
+      '        \\/'];
+
+    domAnimator.addFrame(burger);
+    domAnimator.addFrame(drink);
+    domAnimator.addFrame(melon);
+    domAnimator.addFrame(icecream);
+    domAnimator.animate(1000);
+
+    return React.DOM.div();
+  }
+});
+
+module.exports = ElemeDomAnimator;
---
+++ index.js
@@ -0,0 +1,1 @@
+exports.ElemeDomAnimator = require('./ElemeDomAnimator.js');
